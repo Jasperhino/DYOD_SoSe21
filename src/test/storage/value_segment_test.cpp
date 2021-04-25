d2370afd778b90211138012a4ed886281fcf95eb
bengelhaupt <code@bengelhaupt.com>
2021-04-25 14:31:13 +0200

    refine tests

--- src/test/storage/value_segment_test.cpp
+++ src/test/storage/value_segment_test.cpp
@@ -33,14 +33,18 @@ TEST_F(StorageValueSegmentTest, AddValueOfSameType) {
   EXPECT_EQ(double_value_segment.size(), 1u);
 }
 
-TEST_F(StorageValueSegmentTest, GetValue) {
-  /* Somehow does not work
+TEST_F(StorageValueSegmentTest, GetValues) {
+  int_value_segment.append(3);
+  EXPECT_EQ(int_value_segment.values().at(0), 3);
   EXPECT_EQ(type_cast<int>(int_value_segment[ChunkOffset{0}]), 3);
 
+  string_value_segment.append("Hello");
+  EXPECT_EQ(string_value_segment.values().at(0), "Hello");
   EXPECT_EQ(type_cast<std::string>(string_value_segment[ChunkOffset{0}]), "Hello");
 
+  double_value_segment.append(3.14);
+  EXPECT_EQ(double_value_segment.values().at(0), 3.14);
   EXPECT_EQ(type_cast<double>(double_value_segment[ChunkOffset{0}]), 3.14);
-   */
 }
 
 TEST_F(StorageValueSegmentTest, AddValueOfDifferentType) {
